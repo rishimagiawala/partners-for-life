f82bbc2090fac866e7a113d1cd4ea93f4b3f6b79
rishimagiawala <rishimagiawala@gmail.com>
2021-06-10 20:30:22 -0400

    Completed Login Screen
    Login Screen UI is officially done: just need to tweak functionality for NodeJs and buttons
    
    I also added a logo but its not final

--- Login.tsx
+++ Login.tsx
@@ -1,73 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
-
- 
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert, StyleSheet, Text, View, Image } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { lightBlue100 } from "react-native-paper/lib/typescript/styles/colors";
+const logo = require("./assets/logo.png");
 export default function Login(prop: any) {
-  const [password, setPassword] = React.useState('');
-  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState("");
   const [hidePassword, setHidePassword] = React.useState(true);
-  
+
+  ///////////////////////////////////////////////////////////////////
+
+  /////////////////////////////////////////////////////////////////////
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeTitle}>Welcome to Partner for Life</Text>
-      <TextInput style= {styles.emailInput}
-      label="Email"
-      value = {email}
+      <Image style={styles.logo} source={logo} />
       
-      onChangeText = {(email) => setEmail(email)}
-      
-    />
-    <TextInput style= {styles.passwordInput}
-      label="Password"
-      secureTextEntry = {hidePassword}
-      value = {password}
-      onChangeText = {(password) => setPassword(password)}
-      right={<TextInput.Icon name="eye" onPress={()=>{
-        setHidePassword(!hidePassword)
-      }} />}
-    />
-    <Button style={styles.loginButton} icon="login" mode="contained" onPress={() => {
-      Alert.alert('Email: ' + email + ' Password: ' + password);
-      console.log('Email: ' + email + ' Password: ' + password);
-    }}>
-    Log In
-  </Button>
-    
-    
+      <TextInput
+        theme={{ colors: { primary: "#118ab2" } }}
+        selectionColor="#118ab2"
+        outlineColor="#118ab2"
+        underlineColor="#118ab2"
+        style={styles.emailInput}
+        label="Email"
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+      />
+      <TextInput
+        theme={{ colors: { primary: "#118ab2" } }}
+        selectionColor="#118ab2"
+        outlineColor="#118ab2"
+        underlineColor="#118ab2"
+        style={styles.passwordInput}
+        label="Password"
+        secureTextEntry={hidePassword}
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        right={
+          <TextInput.Icon
+            name="eye"
+            onPress={() => {
+              setHidePassword(!hidePassword);
+            }}
+          />
+        }
+      />
+      <Button
+        color="#118ab2"
+        style={styles.loginButton}
+        icon="login"
+        mode="contained"
+        onPress={() => {
+          console.log("Email: " + email + " Password: " + password);
+          const Http = new XMLHttpRequest();
+          const url = "http://192.168.1.26:8000/";
+          Http.open("POST", url);
+          Http.setRequestHeader("Content-Type", "application/json");
+          Http.send(
+            JSON.stringify({
+              login: {
+                email: email,
+                password: password,
+              },
+            })
+          );
+
+          Http.onreadystatechange = (e) => {
+            console.log(Http.responseText);
+          };
+        }}
+      >
+        Log In
+      </Button>
+
+      <Text style={styles.forgotPassword}>Forgot Password?</Text>
+      <Button
+        dark={true}
+        color="#06d6a0"
+        style={styles.signUpButton}
+        icon="account-plus-outline"
+        mode="contained"
+      >
+        Sign Up
+      </Button>
+      {/* <View style={{ width: 330,flexDirection: "row", alignItems: "center" }}>
+        <View style={{flex: 1, height: 1, backgroundColor: "black" }} />
+        <View>
+          <Text style={{ width: 50, textAlign: "center" }}>OR</Text>
+        </View>
+        <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
+      </View> */}
     </View>
   );
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f7f7f7",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  passwordInput:{
-   height: 50,
-   width: 350,
-   marginVertical: 15
-   
-  },
- emailInput:{
+  passwordInput: {
     height: 50,
     width: 350,
-    marginVertical: 15
-    
-   },
-   loginButton: {
-    marginVertical: 15
-   },
-   welcomeTitle: {
-     position: 'relative',
-     bottom: 90,
-    marginBottom: 50,
-     fontSize: 25
-     
-   }
+    marginVertical: 15,
+  },
+  emailInput: {
+    height: 50,
+    width: 350,
+    marginVertical: 15,
+    color: "#118ab2",
+  },
+  loginButton: {
+    marginVertical: 15,
+    width: 350,
+    borderRadius: 20,
+  },
+  signUpButton: {
+    marginTop: 30,
+    width: 350,
+    borderRadius: 20,
+  },
+  welcomeTitle: {
+    position: "relative",
+    bottom: 90,
+
+    fontSize: 25,
+  },
+  forgotPassword: {
+    color: "#118ab2",
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  logo:{
+    position:'relative',
+    bottom: 30
+  }
 });
